b639aeadc299e8b03d288923c63c493e050436ed
yattorie <pyryryqw882@gmail.com>
2026-01-23 04:13:40 +0300

    feat: add admin forms for custom User model

---
+++ apps/accounts/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from .models import User
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("email",)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password1") != cleaned_data.get("password2"):
+            raise forms.ValidationError("Passwords do not match")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        label="Password",
+        help_text="Raw passwords are not stored, so there is no way to see this password."
+    )
+
+    class Meta:
+        model = User
+        fields = "__all__"
